ce9da86d33cf521e670e68faf17cf615bfef4f13
MikesVibe <miki889-99@wp.pl>
2022-10-18 23:46:36 +0200

    Task solved - ostatnia niezerowa cyfra silni'

---
+++ OSTSIL - Ostania niezerowa cyfra silni.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	int input, factorial;
+	std::string factorial_string;
+
+	int number_of_tests;
+	std::cin >> number_of_tests;
+	unsigned long long N = 0;
+
+	for (int j = 0; j < number_of_tests; j++)
+	{
+		std::cin >> N;
+		unsigned long long x = 1;
+		for (unsigned long long n = 2; n <= N; ++n)
+		{
+			x *= n;
+			while (!(x % 10)) x /= 10;
+			x %= 1000000000LL;
+		}
+		std::cout << x % 10 << std::endl;
+	}
+	return 0;
+}
