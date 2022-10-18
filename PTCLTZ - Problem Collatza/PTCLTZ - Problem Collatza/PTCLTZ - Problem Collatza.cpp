b1de3238294784755343790ff647a3843438a920
MikesVibe <miki889-99@wp.pl>
2022-10-18 23:54:51 +0200

    Fucntion name correction

--- PTCLTZ - Problem Collatza/PTCLTZ - Problem Collatza/PTCLTZ - Problem Collatza.cpp
+++ PTCLTZ - Problem Collatza/PTCLTZ - Problem Collatza/PTCLTZ - Problem Collatza.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 
-int recurention(int S, int iteration = 0);
+int recursion(int S, int iteration = 0);
 
 int main()
 {
@@ -11,19 +11,18 @@ int main()
     for (int i = 0; i < number_of_tests; i++)
     {
         std::cin >> input;
-        std::cout << recurention(input) << "\n";
-
+        std::cout << recursion(input) << "\n";
     }
 
     return 0;
 }
 
-int recurention(int S, int iteration)
+int recursion(int S, int iteration)
 {
     if (S == 1)
         return iteration;
 
     int Xn1 = S;
     Xn1 = (S % 2 == 1) ? 3 * S + 1 : Xn1 = S / 2;
-    return recurention(Xn1, ++iteration);
+    return recursion(Xn1, ++iteration);
 }
